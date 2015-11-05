b06c777e1be502dbd1f56b1f2e9a539e9d25adda
Emilio <emilio2hd@gmail.com>
2015-11-05 00:31:58 -0200

    Hangperson class implementation

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,5 +1,5 @@
 class HangpersonGame
-
+  attr_accessor :word, :guesses, :wrong_guesses
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
@@ -7,9 +7,46 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @game_status = :play
+  end
+
+  def guess(letter)
+    raise ArgumentError if "#{letter}".empty? || !letter.match(/[a-z]/i)
+
+    letter.downcase!
+
+    if @word.include?(letter) && !@guesses.include?(letter)
+      @guesses += letter
+      @game_status = :win if word_with_guesses == @word
+      return true
+    elsif !@guesses.include?(letter) && !@wrong_guesses.include?(letter)
+      @wrong_guesses  += letter
+      @game_status = :lose if @wrong_guesses.length == 7
+      return true
+    end
+
+    return false
+  end
+
+  def word_with_guesses
+    word_guessed = "-" * @word.length
+
+    (0..(word.length-1)).each do |i|
+      if @guesses.include?(word[i])
+        word_guessed[i] = word[i]
+      end
+    end
+
+    word_guessed
+  end
+
+  def check_win_or_lose
+    @game_status
   end
 
   def self.get_random_word
